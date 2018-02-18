af78bdd5aafb8cb823c32c30de493048a39c80c1
wisthler <wisthler@gmail.com>
2018-02-18 22:43:57 +0100

    preparing db connexion - renaming field to avoid reserved keyword

--- src/main/java/be/shoktan/alliance/tranquille/model/GuildLogEvent.java
+++ src/main/java/be/shoktan/alliance/tranquille/model/GuildLogEvent.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.StringUtils;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -23,6 +24,8 @@ public class GuildLogEvent {
     //2017-12-31T15:03:59.000Z
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     private LocalDateTime time;
+
+    @Column(name = "targetUser")
     private String user;
     private GuildLogEventType type;
 
