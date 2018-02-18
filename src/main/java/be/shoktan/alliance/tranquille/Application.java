4222cab317841495a583a0d5b35cc7db385a85a4
wisthler <wisthler@gmail.com>
2018-02-18 20:12:57 +0100

    preparing db connexion

--- src/main/java/be/shoktan/alliance/tranquille/Application.java
+++ src/main/java/be/shoktan/alliance/tranquille/Application.java
@@ -1,13 +1,20 @@
 package be.shoktan.alliance.tranquille;
 
+import java.sql.SQLException;
 import java.util.Arrays;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
+
 
 @SpringBootApplication
 public class Application {
@@ -37,6 +44,20 @@ public class Application {
             }
 
         };
+    }
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        if (dbUrl == null || dbUrl.isEmpty()) {
+            return new HikariDataSource();
+        } else {
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
     }
 
 }
